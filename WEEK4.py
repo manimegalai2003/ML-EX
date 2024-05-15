b638b35b5486aa508d3a259027c71053a8ff6357
manimegalai2003 <163968596+manimegalai2003@users.noreply.github.com>
2024-05-15 13:53:55 +0530

    Update and rename README.md to WEEK4.py

---
+++ WEEK4.py
@@ -0,0 +1,61 @@
+import streamlit as st
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+# Load data from CSV
+@st.cache
+def load_data():
+    data = pd.read_csv('tennisdata.csv')
+    return data
+
+data = load_data()
+
+st.title("Tennis Data Analysis")
+
+# Display the first 5 rows of the data
+st.write("The first 5 values of data are:")
+st.write(data.head())
+
+# Obtain Train data and Train output
+X = data.iloc[:,:-1]
+st.write("\nThe First 5 values of train data are:")
+st.write(X.head())
+
+y = data.iloc[:,-1]
+st.write("\nThe first 5 values of Train output are:")
+st.write(y.head())
+
+# Convert categorical features to numerical values
+le_outlook = LabelEncoder()
+X['Outlook'] = le_outlook.fit_transform(X['Outlook'])
+
+le_Temperature = LabelEncoder()
+X['Temperature'] = le_Temperature.fit_transform(X['Temperature'])
+
+le_Humidity = LabelEncoder()
+X['Humidity'] = le_Humidity.fit_transform(X['Humidity'])
+
+le_Windy = LabelEncoder()
+X['Windy'] = le_Windy.fit_transform(X['Windy'])
+
+st.write("\nNow the Train data is:")
+st.write(X.head())
+
+le_PlayTennis = LabelEncoder()
+y = le_PlayTennis.fit_transform(y)
+st.write("\nNow the Train output is:")
+st.write(y)
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
+
+# Train the classifier
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+
+# Evaluate the classifier
+accuracy = accuracy_score(classifier.predict(X_test), y_test)
+st.write("\nAccuracy is:", accuracy)
